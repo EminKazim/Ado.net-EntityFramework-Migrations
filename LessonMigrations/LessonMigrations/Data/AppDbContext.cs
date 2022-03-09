4d137453bbe71494e00f0b230e715d23059d729e
Emin <eminjk@code.edu.az>
2022-03-09 13:25:40 -0800

    hometask

---
+++ LessonMigrations/LessonMigrations/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LessonMigrations.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace LessonMigrations.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+               
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SliderDetail> SliderDetails { get; set; }
+    }
+}
---
+++ LessonMigrations/LessonMigrations/ViewModels/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using LessonMigrations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonMigrations.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+        public SliderDetail Detail { get; set; }
+    }
+}
